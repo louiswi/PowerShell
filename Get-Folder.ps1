129745ce17f5c5403059a4d5103636ed005f314b
louiswi <lu814478913@gmail.com>
2016-11-29 09:43:39 -0600

    Add files via upload

---
+++ Get-Folder.ps1
@@ -0,0 +1,14 @@
+﻿Function Get-Folder($initialDirectory="MyComputer")
+
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $foldername.rootfolder = "MyComputer"
+
+    if($foldername.ShowDialog() -eq "OK")
+    {
+        $folder += $foldername.SelectedPath
+    }
+    return $folder
+}
